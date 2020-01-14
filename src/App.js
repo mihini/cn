992b3f02a04c8650fe4108a1c1007b18d66ca5f6
mihini <manina103@hotmail.com>
2020-01-14 15:53:38 +0100

    removing comments and unused code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import salon from "./salong.json";
 import List from "./Components/List";
 import Salon from "./Components/Salon";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -24,21 +16,6 @@ const App = () => {
           ></Route>
           <Route path="/" render={props => <List {...props} salon={salon} />} />
         </Switch>
-        {/* <List salon={salon}></List> */}
-        {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       </Router>
     </div>
   );

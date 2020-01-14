4dcb566cd806d33ba471775e943a0312ddf2f597
mihini <manina103@hotmail.com>
2020-01-14 15:46:35 +0100

    Updates

---
+++ precache-manifest.ef83a76b5ceb43d3bc6331e1f8c1e774.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39fb56e83d45cce37d403c684750607",
+    "url": "//mihini.github.io/cn/index.html"
+  },
+  {
+    "revision": "98c8be435109ae983826",
+    "url": "//mihini.github.io/cn/static/css/main.9819eb04.chunk.css"
+  },
+  {
+    "revision": "43e403eb87189c4dd1d2",
+    "url": "//mihini.github.io/cn/static/js/2.c13f0036.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "//mihini.github.io/cn/static/js/2.c13f0036.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98c8be435109ae983826",
+    "url": "//mihini.github.io/cn/static/js/main.1243ae62.chunk.js"
+  },
+  {
+    "revision": "9411b20ea6dbd78abdb9",
+    "url": "//mihini.github.io/cn/static/js/runtime-main.e3112f0e.js"
+  },
+  {
+    "revision": "1b46a122acd7a09b010120f2196a247a",
+    "url": "//mihini.github.io/cn/static/media/arrow.1b46a122.svg"
+  },
+  {
+    "revision": "1865d748e72c14627d90a97b76c5aac9",
+    "url": "//mihini.github.io/cn/static/media/arrowbig.1865d748.svg"
+  },
+  {
+    "revision": "b29eb6b8b11d70d3b22d62ea15f6fcdb",
+    "url": "//mihini.github.io/cn/static/media/clock.b29eb6b8.svg"
+  },
+  {
+    "revision": "4c9473f8ebdbabc6a901acce29d037c7",
+    "url": "//mihini.github.io/cn/static/media/filter.4c9473f8.svg"
+  },
+  {
+    "revision": "aff4e59d46867831fdbe8d2e462784ac",
+    "url": "//mihini.github.io/cn/static/media/font-bureau.aff4e59d.otf"
+  },
+  {
+    "revision": "2ffa8a52471ba3c3926b0c91a5bdda90",
+    "url": "//mihini.github.io/cn/static/media/globe.2ffa8a52.svg"
+  },
+  {
+    "revision": "cadc4e5255fb9c80f0a2432f664342df",
+    "url": "//mihini.github.io/cn/static/media/hair.cadc4e52.jpg"
+  },
+  {
+    "revision": "7c6c893deeb5f9c8fe02487a8952ac19",
+    "url": "//mihini.github.io/cn/static/media/heart.7c6c893d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//mihini.github.io/cn/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5bad3fe9ed84355a3085a3864627c2eb",
+    "url": "//mihini.github.io/cn/static/media/phone.5bad3fe9.svg"
+  },
+  {
+    "revision": "518eebec6b2b234861165d2b998a7321",
+    "url": "//mihini.github.io/cn/static/media/pin.518eebec.svg"
+  },
+  {
+    "revision": "407c5db31d52f3b9b69060818779816f",
+    "url": "//mihini.github.io/cn/static/media/star.407c5db3.svg"
+  },
+  {
+    "revision": "9a5d93ab08e07b55e107551ee70eec16",
+    "url": "//mihini.github.io/cn/static/media/stargold.9a5d93ab.svg"
+  }
+]);
